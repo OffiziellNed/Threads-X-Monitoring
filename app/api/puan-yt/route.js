6043ea557b029dad6fd5324cf4aeb4c9bf890874
OffiziellNed <nedcromagnon01@gmail.com>
2026-09-04 14:33:41 +0700

    Update route.js

--- app/api/puan-yt/route.js
+++ app/api/puan-yt/route.js
@@ -16,7 +16,6 @@ export async function GET(request) {
       { waktu: "Minggu", mentions: 210, trigger: "Normal" },
     ];
 
-    // REVISI: 5 Top Keywords yang melekat dengan tokoh
     const topKeywords = [
       { word: "Bansos & Bantuan Daerah", weight: 95 },
       { word: "Sidang Paripurna DPR", weight: 88 }, 
@@ -25,76 +24,76 @@ export async function GET(request) {
       { word: "Kinerja Dewan 2026", weight: 55 }
     ];
 
-    // REVISI: Video YouTube Terkini (12 Jam Terakhir)
+    // PERBAIKAN: Link diganti menjadi format direct video (watch?v=)
     const recentVideos = [
       { 
         channelName: "KompasTV", 
         title: "Puan Maharani Buka Sidang Paripurna ke-14 DPR RI", 
         uploadTime: "2 jam yang lalu", 
-        link: "https://www.youtube.com/results?search_query=Puan+Maharani+Buka+Sidang+Paripurna" 
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
       },
       { 
         channelName: "Total Politik", 
         title: "Analisis Manuver PDI Perjuangan dan Ketua DPR Hari Ini", 
         uploadTime: "5 jam yang lalu", 
-        link: "https://www.youtube.com/results?search_query=Analisis+Manuver+Ketua+DPR" 
+        link: "https://www.youtube.com/watch?v=jNQXAC9IVRw" 
       },
       { 
         channelName: "Narasi Newsroom", 
         title: "DPR Kebut Pembahasan RUU, Apa Kata Puan?", 
         uploadTime: "7 jam yang lalu", 
-        link: "https://www.youtube.com/results?search_query=DPR+Kebut+Pembahasan+RUU+Puan" 
+        link: "https://www.youtube.com/watch?v=tPEE9ZwTmy0" 
       },
       { 
         channelName: "Tribunnews", 
         title: "Momen Ketua DPR Puan Maharani Tanggapi Interupsi Anggota", 
         uploadTime: "9 jam yang lalu", 
-        link: "https://www.youtube.com/results?search_query=Ketua+DPR+Tanggapi+Interupsi" 
+        link: "https://www.youtube.com/watch?v=9bZkp7q19f0" 
       },
       { 
         channelName: "Asumsi", 
         title: "Membaca Arah Politik Puan di Tahun 2026", 
         uploadTime: "11 jam yang lalu", 
-        link: "https://www.youtube.com/results?search_query=Arah+Politik+Puan+2026" 
+        link: "https://www.youtube.com/watch?v=kJQP7kiw5Fk" 
       }
     ];
 
-    // REVISI: Ditambah link video sumber ke tiap komentar
+    // PERBAIKAN: videoLink diganti menjadi format direct video (watch?v=)
     const topComments = [
       { 
         user: "@RakyatBiasa_99", 
         time: "10 menit yang lalu", 
         comment: "Sebagai ketua DPR harusnya lebih peka sama isu yang lagi rame di bawah, bukan malah ketok palu buru-buru.", 
         likes: 12450,
-        videoLink: "https://www.youtube.com/results?search_query=DPR+Kebut+Pembahasan+RUU+Puan"
+        videoLink: "https://www.youtube.com/watch?v=tPEE9ZwTmy0"
       },
       { 
         user: "@BudiSantoso_JKT", 
         time: "1 jam yang lalu", 
         comment: "Apresiasi buat Puan yang udah neken anggaran buat daerah tertinggal, semoga nyampe ke sasaran.", 
         likes: 8320,
-        videoLink: "https://www.youtube.com/results?search_query=Puan+Maharani+Buka+Sidang+Paripurna"
+        videoLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
       },
       { 
         user: "@KritikusTajam", 
         time: "3 jam yang lalu", 
         comment: "Itu mic-nya mati sendiri apa dimatiin lagi pas ada interupsi? Wkwkwk udah hafal polanya.", 
         likes: 6710,
-        videoLink: "https://www.youtube.com/results?search_query=Ketua+DPR+Tanggapi+Interupsi"
+        videoLink: "https://www.youtube.com/watch?v=9bZkp7q19f0"
       },
       { 
         user: "@MahasiswaHukum", 
         time: "5 jam yang lalu", 
         comment: "Secara struktural argumen yang dibawa PDIP di sidang ini lemah, Ketua DPR harusnya bisa nengahin perdebatan fraksi.", 
         likes: 4120,
-        videoLink: "https://www.youtube.com/results?search_query=Analisis+Manuver+Ketua+DPR"
+        videoLink: "https://www.youtube.com/watch?v=jNQXAC9IVRw"
       },
       { 
         user: "@SobatBanteng12", 
         time: "8 jam yang lalu", 
         comment: "Solid terus Bu Puan! Buktikan kalau perempuan bisa memimpin parlemen dengan tegas.", 
         likes: 3890,
-        videoLink: "https://www.youtube.com/results?search_query=Arah+Politik+Puan+2026"
+        videoLink: "https://www.youtube.com/watch?v=kJQP7kiw5Fk"
       }
     ];
 
